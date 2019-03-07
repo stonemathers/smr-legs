93a931427f23e42e142afc96841a4cec1453f4a5
Stone Mathers <smm117@duke.edu>
2019-03-07 12:11:25 -0500

    Visualized as continuous portion, not individual 'Mountains'

--- visualization.js
+++ visualization.js
@@ -8,31 +8,31 @@ const GROUND_COLOR = "#9B7653";
 //Title constants
 const TITLE_FONT_SIZE = 40;
 const TITLE_Y = 50;
-const TITLE_MSG = "140-mile Smoky Mountain Relay Legs"
+const TITLE_MSG = "140-mile Smoky Leg Relay Legs"
 
-//Mountain Color Pallette
+//Leg Color Pallette
 const EASIEST_COLOR = "#00B800";
 const HARDEST_COLOR = "#FE0000";
 
 //Data vars
 let legData;
-let mountains;
+let legs;
 let maxDifficulty = 0;
 
-//Mountain constants
-const WIDTH_MULT = 50;
-const HEIGHT_MULT = 0.2;
+//Leg constants
+const WIDTH_MULT = 100;
+const HEIGHT_MULT = 0.1;
 const SCROLL_SPEED = 10;
 const MOUNT_BUFFER = 300;
 
-//Mountain label strings
+//Leg label strings
 const LEG_LABEL = "Leg #";
 const DIST_LABEL = "Distance: ";
 const MI_LABEL = " mi";
 const CLIMB_LABEL = "Total Climb: ";
 const FT_LABEL = " ft"
 const DIFF_LABEL = "Difficulty: "
-const CLIMB_THRESH = 450;
+const LABEL_HEIGHT_THRESH = 100;
 const ABOVE_TEXT_BUFF = 5;
 const LABEL_FONT_SIZE = 12;
 
@@ -40,7 +40,7 @@ const LABEL_FONT_SIZE = 12;
 * Run once before site loads
 */
 function preload(){
-    legData = loadTable("assets/csv/legs.csv", "header");
+    legData = loadJSON("assets/json/legs.json");
 }
 
 /*
@@ -48,7 +48,7 @@ function preload(){
 */
 function setup(){
     createCanvas(windowWidth, windowHeight);
-    initMountains();
+    initLegs();
 }
 
 /*
@@ -65,23 +65,23 @@ function draw(){
     textSize(TITLE_FONT_SIZE);
     text(TITLE_MSG, windowWidth/2, TITLE_Y);
 
-    //Move Mountains
-    if(keyIsDown(LEFT_ARROW) && mountains[0].x < MOUNT_BUFFER){
-        for(let i = 0; i < mountains.length; i++){
-            mountains[i].move(SCROLL_SPEED);
+    //Move Legs
+    if(keyIsDown(LEFT_ARROW) && legs[0].x < MOUNT_BUFFER){
+        for(let i = 0; i < legs.length; i++){
+            legs[i].move(SCROLL_SPEED);
         }
     }else if(keyIsDown(RIGHT_ARROW) &&
-        (mountains[mountains.length - 1].x + mountains[mountains.length - 1].width) > (width - MOUNT_BUFFER)){
-        for(let i = 0; i < mountains.length; i++){
-            mountains[i].move(-SCROLL_SPEED);
+        (legs[legs.length - 1].x + legs[legs.length - 1].legWidth) > (width - MOUNT_BUFFER)){
+        for(let i = 0; i < legs.length; i++){
+            legs[i].move(-SCROLL_SPEED);
         }
     }
 
-    //Draw Mountains
+    //Draw Legs
     colorMode(HSB);
-    for(let i = 0; i < mountains.length; i++){
-        if(mountains[i].stillDisplaying()){
-            mountains[i].display();
+    for(let i = 0; i < legs.length; i++){
+        if(legs[i].stillDisplaying()){
+            legs[i].display();
         }
     }
 
@@ -103,11 +103,11 @@ function windowResized() {
 * Run when mouse is scrolled
 */
 function mouseWheel(event){
-    //Move Mountains
-    if((event.delta < 0 && mountains[0].x < MOUNT_BUFFER) || 
-        (event.delta > 0 && (mountains[mountains.length - 1].x + mountains[mountains.length - 1].width) > (width - MOUNT_BUFFER))){
-        for(let i = 0; i < mountains.length; i++){
-            mountains[i].move(-event.delta);
+    //Move Legs
+    if((event.delta < 0 && legs[0].x < MOUNT_BUFFER) || 
+        (event.delta > 0 && (legs[legs.length - 1].x + legs[legs.length - 1].legWidth) > (width - MOUNT_BUFFER))){
+        for(let i = 0; i < legs.length; i++){
+            legs[i].move(-event.delta);
         }
     }
 
@@ -116,47 +116,54 @@ function mouseWheel(event){
 }
 
 /*
-* Initializes the array of Mountain object from the SMR legs data
+* Initializes the array of Leg objects from the SMR legs data
 */
-function initMountains(){
+function initLegs(){
     let currX = MOUNT_BUFFER;
-    mountains = [];
+    legs = [];
 
-    for(let i = 0; i < legData.getRowCount(); i++){
+    for(let i = 0; i < legData.leg_list.length; i++){
         //Get data
-        let row = legData.getRow(i);
-        let leg = int(row.get("leg_num"));
-        let dist = int(row.get("dist"));
-        let climb = int(row.get("climb_total"));
-        let diff = int(row.get("difficulty"));
+        let leg_num = legData.leg_list[i].leg_num;
+        let dist = legData.leg_list[i].dist;
+        let climb = legData.leg_list[i].climb_total;
+        let diff = legData.leg_list[i].difficulty;
+        let ports = legData.leg_list[i].portions;
 
-        //Calculate height and width
-        let height = climb * HEIGHT_MULT;
-        let width = dist * WIDTH_MULT;
+        //Calculate overall width
+        let legWidth = dist * WIDTH_MULT;
 
-        //Create Mountain
-        mountains.push(new Mountain(leg, currX, height, width, diff));
+        //Create Leg
+        legs.push(new Leg(leg_num, currX, legWidth, diff, climb, ports));
 
         //Update vars
-        currX += width;
+        currX += legWidth;
         maxDifficulty = Math.max(maxDifficulty, diff);
     }
 }
 
 /*
-* Class for Mountain object
+* Class for Leg object
 */
-class Mountain{
-    constructor(legNum, x, height, width, difficulty){
+class Leg{
+    constructor(legNum, x, legWidth, difficulty, climb_total, portions){
         this.legNum = legNum;
         this.x = x;
-        this.height = height;
-        this.width = width;
+        this.legWidth = legWidth;
         this.difficulty = difficulty;
+        this.climb_total = climb_total;
+        this.portions = portions;
+        
+        //Determine highest point
+        let maxH = this.portions[0].start_elev * HEIGHT_MULT;
+        for(let i = 0; i < this.portions.length; i++){
+            maxH = Math.max(maxH, this.portions[i].end_elev * HEIGHT_MULT);
+        }
+        this.height = maxH;
     }
 
     /*
-    * Display Mountain
+    * Display Leg
     */
     display(){
         //Set color
@@ -169,16 +176,15 @@ class Mountain{
             fill(fillColor);
         }
 
-        //Calculate coordinates
-        let x1 = this.x;
-        let y1 = GROUND_HEIGHT;
-        let x2 = x1 + this.width/2;
-        let y2 = y1 - this.height;
-        let x3 = x1 + this.width;
-        let y3 = y1;
-
-        //Draw triangle
-        triangle(x1, y1, x2, y2, x3, y3);
+        //Draw shape
+        beginShape();
+        vertex(this.x, GROUND_HEIGHT);  //bottom left
+        vertex(this.x, GROUND_HEIGHT - (this.portions[0].start_elev * HEIGHT_MULT));    //top left
+        for(let i = 0; i < this.portions.length; i++){
+            vertex(this.x + this.portions[i].end_dist * WIDTH_MULT, GROUND_HEIGHT - (this.portions[i].end_elev * HEIGHT_MULT))   //top
+        }
+        vertex(this.x + this.legWidth, GROUND_HEIGHT); //bottom right
+        endShape(CLOSE);
 
         //Draw label
         if(this.mouseIsOver()){
@@ -187,26 +193,25 @@ class Mountain{
     }
 
     /*
-    * Draws label displaying info about Mountain
+    * Draws label displaying info about Leg
     */
     drawLabel(){
         //Get data
-        let data = legData.getRow(this.legNum);
+        let currLeg = legData.leg_list[this.legNum];
         let num = this.legNum + 1;
-        let dist = data.get("dist");
-        let climb = data.get("climb_total");
+        let dist = currLeg.dist;
 
         //Create string
         let infoString = LEG_LABEL + str(num) + "\n"
             + DIST_LABEL + str(dist) + MI_LABEL + "\n"
-            + CLIMB_LABEL + str(climb) + FT_LABEL + "\n"
-            + DIFF_LABEL + str(data.get("difficulty"));
+            + CLIMB_LABEL + str(this.climb_total) + FT_LABEL + "\n"
+            + DIFF_LABEL + str(this.difficulty);
 
         //Calc position
-        let infoX = this.x + (this.width/2);
+        let infoX = this.x + (this.legWidth/2);
         let infoY;
 
-        if(climb < CLIMB_THRESH){
+        if(this.height < LABEL_HEIGHT_THRESH){
             textAlign(CENTER, BOTTOM);
             infoY = GROUND_HEIGHT - this.height - ABOVE_TEXT_BUFF;
         }else{
@@ -221,30 +226,30 @@ class Mountain{
     }
 
     /*
-    * Move the Mountain the given number of pixels
+    * Move the Leg the given number of pixels
     *
     * Input:
-    *   distance - number of pixels to move Mountain
+    *   distance - number of pixels to move Leg
     */
     move(distance){
         this.x += distance;
     }
 
     /*
-    * Determine whether Mountain is still on the screen
+    * Determine whether Leg is still on the screen
     *
     * Output:
-    *   true if Mountain is still on screen, false if not
+    *   true if Leg is still on screen, false if not
     */
    stillDisplaying(){
-        return this.x < width && this.x + this.width > 0;
+        return this.x < width && this.x + this.legWidth > 0;
     }
 
     /*
-    * Return true if mouse is over the Mountain
+    * Return true if mouse is over the Leg
     */
     mouseIsOver(){
-        return (mouseX >= this.x) && (mouseX <= this.x + this.width) && (mouseY <= GROUND_HEIGHT) && (mouseY >= GROUND_HEIGHT - this.height);
+        return (mouseX >= this.x) && (mouseX <= this.x + this.legWidth) && (mouseY <= GROUND_HEIGHT) && (mouseY >= GROUND_HEIGHT - this.height);
     }
 
 }
